53c9ac28cd230b54068d2f0004f6fa571921ac5d
limwc <limwc@MA22N015>
2022-12-05 14:29:37 +0800

    3

--- HelloWord/Controllers/WeatherForecastController.cs
+++ HelloWord/Controllers/WeatherForecastController.cs
@@ -18,10 +18,10 @@ namespace HelloWord.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast2")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 8).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 7).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
